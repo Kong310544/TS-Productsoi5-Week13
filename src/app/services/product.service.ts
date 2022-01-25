3a6cc53fa3fd7a807ea50c14ca933979a9592b7d
Kong310544 <kong310544@gmail.com>
2022-01-25 17:47:58 +0700

    Ts-Week13

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs';
+import { environment } from 'src/environments/environment'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private url = `${environment.serviceUrl}/product`
+  constructor(private http: HttpClient) { }
+
+  //get All Product
+  getProduct(): any{
+    return this.http.get<any>(this.url);
+  }
+
+  //Get by id
+  getProductById(id:any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.get<any>(getUrl);
+  }
+
+  addProduct(product: any){
+    return this.http.post<any>(this.url, product)
+    .pipe(map((res)=>{
+      return res;
+    }));
+  }
+
+
+  //Update
+  updateProduct(product: any,id: any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.put<any>(getUrl, product)
+    .pipe(map((res)=>{
+      return res;
+    }));
+  }
+
+  //Delete
+  deleteProduct(id: any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.delete<any>(getUrl);
+  }
+
+  
+
+
+
+
+
+
+}
